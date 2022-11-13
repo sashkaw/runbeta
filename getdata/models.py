34623a8bc02d13621bb01f29a03acbe148f8b0ff
duraznoj <sashka.warner@gmail.com>
2022-11-13 01:58:57 -0800

    Consolidate model data into Activity class

--- getdata/models.py
+++ getdata/models.py
@@ -1,73 +1,78 @@
-#from django.db import models
+# Django imports
 from django.shortcuts import redirect
 from django.utils import timezone
 from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.gis.db import models
-#from django.conf import settings
+from django.contrib.postgres.fields import ArrayField
 
+# Load libraries
 import time
-#import pickle
-#import os
-#import dotenv
-#from dotenv import load_dotenv
 import datetime
 from pandas import to_numeric
 import polyline
-#from pathlib import Path
-
-# Create your models here.
 
 # Customize model initiation with use of manager -> preferred for Django instead of overriding '__init__'
-class RouteManager(models.Manager):
-  def create_route(self, user, route, date, distance, line):
-    new_route = self.create(
+class ActivityManager(models.Manager):
+  def create_activity(self, user, activity, start_date, created_date, line):
+    new_activity = self.create(
       user_id=user,
-      route_id=route,
-      date=date,
-      distance=distance,
-      line=line,
+      activity_id=activity,
+      start_date=start_date,
+      created_date=created_date,
+      line=line
     )
     
-    # Return the route object 
-    return new_route
+    # Return the activity object 
+    return new_activity
 
-# Class Route
-class Route(models.Model):
-  # User name --> not sure if required
-  user_id = models.ForeignKey(User, on_delete = models.CASCADE)
-  # Name of route
-  route_id = models.CharField(max_length = 200)
-  # Route date
-  date = models.DateTimeField("date completed")
-  # Route length
-  distance = models.IntegerField()
+# Class Activity
+class Activity(models.Model):
+
+  # User name
+  user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+  # Name of activity
+  activity_id = models.CharField(max_length=200)
+  
+  # Name of route (shouldn't need this?)
+  #route_id = models.CharField(max_length=200)
+
+  # Activity date
+  start_date = models.DateTimeField(null=True, help_text="date started")
+
+  # Date added to RunBeta database
+  created_date = models.DateTimeField(null=True, help_text="date added to database")
+
+  # Activity stream data
+  time = ArrayField(base_field=models.TimeField(null=True, blank=True, help_text="time elapsed"), null=True)
+  latlng = ArrayField(base_field=models.PointField(null=True, blank=True, help_text="latlng position"), null=True)
+  distance = ArrayField(base_field=models.FloatField(null=True, blank=True, help_text="distance from start"), null=True)
+  altitude = ArrayField(base_field=models.FloatField(null=True, blank=True, help_text="time elapsed"), null=True)
+  velocity_smooth = ArrayField(base_field=models.FloatField(null=True, blank=True, help_text="velocity smooth"), null=True)
+  heartrate = ArrayField(base_field=models.FloatField(null=True, blank=True, help_text="heart rate"), null=True)
+  cadence = ArrayField(base_field=models.FloatField(null=True, blank=True, help_text="cadence"), null=True)
+  watts = ArrayField(base_field=models.FloatField(null=True, blank=True, help_text="watts"), null=True)
+  temp = ArrayField(base_field=models.FloatField(null=True, blank=True, help_text="temperature"), null=True)
+  moving = ArrayField(base_field=models.FloatField(null=True, blank=True, help_text="moving"), null=True)
+  grade_smooth = ArrayField(base_field=models.FloatField(null=True, blank=True, help_text="grade smooth"), null=True)
+
+  # Elevation sampling points
+  elevation_sampling_points = ArrayField(base_field=models.PointField(null=True, blank=True, help_text="elevation sampling points"), null=True)
+
+  #Elevation data
+  elevation = ArrayField(base_field=models.FloatField(null=True, blank=True, help_text="elevation values"), null=True)
+  
   # Spatial geometry of route
-  line = models.LineStringField()
-  # Use RouteManager to initialize Route object with data
-  objects = RouteManager()
+  line = models.LineStringField(null=True, blank=True, help_text="route")
+  
+  # Use manager to initialize Activity object with data
+  objects = ActivityManager()
+
+  # Define unique combination of values for user_id and route_id to avoid duplicates
+  # Note: This migration will only work if you delete previous duplicate entries in the database
+  class Meta:
+    unique_together = ["user_id", "activity_id"]
 
   def __str__(self):
-    return self.route_id
-
-
-class Elevation(models.Model):
-
-  # Route name
-  route_id = models.ForeignKey(Route, on_delete = models.CASCADE)
-
-  # Define fields for elevation data
-  # elevation_x_coord = models.FloatField()
-  # elevation_y_coord = models.FloatField()
-  # elevation_z_coord = models.FloatField()
-
-  # Define field to 
-  xyz_point = models.PointField()
-   
-  def __str__(self):  
-    return self.route_id
-
-  def getElevationData():
-    # TODO: add method for getting x,y,z data from strava and calling Point(x, y, z)
-    # from GEOS api docs: class Point(x=None, y=None, z=None, srid=None)¶
-    pass
+    return self.activity_id
